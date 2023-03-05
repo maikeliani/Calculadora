c2afd7412b962d6efcfcee8554d47ccad68fbed6
maikeliani <maikel.iani31@gmail.com>
2023-03-05 00:12:59 -0300

    criadas funcoes somar, subtrair, multiplicar e dividir. Alterado retorno da funcao menu

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -8,7 +8,7 @@ internal class Program
         double n1, n2;
         int opcao = 0;
 
-        void Menu()
+        int Menu()
         {
             Console.Clear();
             Console.WriteLine(" 1 - SOMAR");
@@ -17,33 +17,62 @@ internal class Program
             Console.WriteLine(" 4 - DIVIDIR");
             Console.WriteLine(" 5 - SAIR");
             Console.WriteLine("Escolha uma opcao: ");
-            opcao = int.Parse(Console.ReadLine());
+            return int.Parse(Console.ReadLine());
+            
+        }
 
+        double somar(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        double subtrair(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        double multiplicar(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        double dividir(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+
+
+        do
+        {
+
+            opcao = Menu();
+            Console.WriteLine(" Informe o primeiro numero: ");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Informe o segundo numero: ");
+            n2 = double.Parse(Console.ReadLine());
+            
             switch (opcao)
             {
 
-                default:
-                    Console.WriteLine("Opção inválida!");
-                    Console.ReadLine();
-                    break;
 
                 case 1:
-                    Console.WriteLine(" A soma dos numeros é: " + (n1 + n2));
+                    Console.WriteLine(" A soma dos numeros é: " + somar(n1,n2).ToString("F2"));
                     Console.ReadLine();
                     break;
 
                 case 2:
-                    Console.WriteLine(" A subtração dos numeros é: " + (n1 - n2));
+                    Console.WriteLine(" A subtração dos numeros é: " + subtrair(n1,n2).ToString("F2"));
                     Console.ReadLine();
                     break;
 
                 case 3:
-                    Console.WriteLine(" A multiplicação  dos numeros é: " + (n1 * n2));
+                    Console.WriteLine(" A multiplicação  dos numeros é: " + multiplicar(n1,n2).ToString("F2"));
                     Console.ReadLine();
                     break;
 
                 case 4:
-                    Console.WriteLine(" A divisao  dos numeros é: " + (n1 / n2));
+                    Console.WriteLine(" A divisao  dos numeros é: " + dividir(n1,n2).ToString("F2"));
                     Console.ReadLine();
                     break;
 
@@ -52,20 +81,13 @@ internal class Program
                     Console.ReadLine();
                     break;
 
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    Console.ReadLine();
+                    break;
+
             }
-        }
-
-
-        Console.WriteLine(" Informe o primeiro numero: ");
-        n1 = double.Parse(Console.ReadLine());
-
-        Console.WriteLine(" Informe o segundo numero: ");
-        n2 = double.Parse(Console.ReadLine());
-
-        while (opcao != 5)
-        {
-            Menu();
-        }
+        } while (opcao != 5);
 
 
     }
